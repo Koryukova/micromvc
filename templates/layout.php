8f90a9d853c50fbd29b2bb6d50ced5750c7987d0
Koryukova <ekaterina.koryukova@gmail.com>
2018-03-13 17:05:48 +0300

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -1,12 +1,14 @@
-
 <!doctype html>
 <html lang="ru">
 <head>
-    <?php require_once 'header.php'?>
+    <?php require_once 'header.php' ?>
 </head>
 <body>
 
-<?php require_once 'views/'.$view?>
+<?php require_once 'navbar.php'; ?>
+<?php require_once 'alerts.php'; ?>
+
+<?php require_once 'views/'.$view ?>
 
 </body>
 </html>
