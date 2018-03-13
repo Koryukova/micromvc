943cc7b28379a4119ae901f37eb1fa5c35e99557
Koryukova <ekaterina.koryukova@gmail.com>
2018-03-13 17:03:26 +0300

    Update weather.php

--- templates/views/weather.php
+++ templates/views/weather.php
@@ -2,10 +2,10 @@
 /**
  * @var $weatherByCity array
  */
-
+dump($weatherByCity);
 ?>
 
-
 <div class="container">
-    <h1>Погода</h1>
+    <h1>Погода:</h1>
+    <p><?= $weatherByCity['name'] ?>: <?= $weatherByCity['weather'][0]['main'] ?></p>
 </div>
