a5ca5961276d05374bdb2dc8ddae37006a04f648
Koryukova <ekaterina.koryukova@gmail.com>
2018-03-13 17:06:32 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,5 @@
-
-
 <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php PROJECT_NAME ?> | <?php= $title ?> </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?= PROJECT_NAME ?> | <?= $title ?></title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
