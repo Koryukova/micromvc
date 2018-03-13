a33a80419fa7166b6403125af8aae33ff8651709
Koryukova <ekaterina.koryukova@gmail.com>
2018-03-13 16:51:57 +0300

    Update Controller.php

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,28 +1,22 @@
 <?php
-
 require_once 'app/models/User.php';
 require_once 'app/components/Weather.php';
-
 class Controller
 {
     public function homeAction() {
         $user = new User();
         $user->setFirstname('Роман');
         $user->setLastname('Ястребов');
-
         $this->render('home.php', 'Главная', [
             'user' => $user,
             'time' => time(),
         ]);
-
     }
-
     public function regAction() {
         $param = $_POST;
-        $user = null;
+        $user = new User();
         if ( isset($param['is_agree']) ) {
             // Создание пользователя
-            $user = new User();
             $user->setFirstname($param['firstname']);
             $user->setLastname($param['lastname']);
             $user->setPassword($param['password']);
@@ -32,35 +26,24 @@ class Controller
             if (isset($param['stack_learn'])) {
                 $user->setStackLearn($param['stack_learn']);
             }
+            Session::addFlash(Session::FLASH_SUCCESS, 'Пользователь успешно зарегистрирован');
         }
         $this->render('reg.php', 'Регистрация', [
             'user' => $user,
-
         ]);
     }
-
     public function weatherAction() {
-        $city = 'Saint%20Petersburg,ru';
-
+        $city = 'Saint Petersburg,ru';
         $weather = new Weather('012e34537b328a78762f56bb13b7ac8c');
-
         $this->render('weather.php', 'Погода', [
             'weatherByCity' => $weather->getWeatherByCity($city),
         ]);
-
     }
-
-
-
-
-
     private function render($view, $title, $param = []) {
         extract($param);
         require_once 'templates/layout.php';
     }
-
     public function route($route) {
         return '/micromvc/?act='.$route;
     }
-
 }
